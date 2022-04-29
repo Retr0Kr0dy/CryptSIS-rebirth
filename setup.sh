675a7a676a27384a59928366de3bc72598f42696
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-04-30 00:18:07 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo 
+echo "  Installing dependencies ..."
+echo
+echo "  | Installing blowfish lib"
+echo
+pip install blowfish
+echo
+echo "  | Installing pycryptodome lib"
+echo
+pip install pycryptodome
+echo
+echo "  | Installing cryptography lib"
+echo
+pip install cryptography
+echo 
+echo "  Succed !!! All depencies are installed"
